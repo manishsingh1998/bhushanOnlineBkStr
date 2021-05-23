b57f5ab00349615b7c65d4601b76990719be1dab
manishsingh1998 <manishsinghadhikari44@gmail.com>
2021-05-23 13:14:30 +0530

    1st commit

---
+++ BhushanOnl9BkStr/BhushanOnl9BkStr/src/main/java/com/fullStack/BhushanOnl9BkStr/BhushanOnl9BkStrApplication.java
@@ -0,0 +1,13 @@
+package com.fullStack.BhushanOnl9BkStr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BhushanOnl9BkStrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BhushanOnl9BkStrApplication.class, args);
+	}
+
+}
---
+++ BhushanOnl9BkStr/BhushanOnl9BkStr/src/test/java/com/fullStack/BhushanOnl9BkStr/BhushanOnl9BkStrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fullStack.BhushanOnl9BkStr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BhushanOnl9BkStrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
