80085a8e90020b141c0766745d5b2eb586dfd78e
Manish Singh <manishsinghadhikari44@gmail.com>
2021-09-28 10:34:02 +0530

    Controller and respository added

---
+++ BhushanOnl9BkStr/BhushanOnl9BkStr/src/main/java/com/fullStack/BhushanOnl9BkStr/controller/BokkStoreController.java
@@ -0,0 +1,8 @@
+package com.fullStack.BhushanOnl9BkStr.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class BokkStoreController {
+
+}
---
+++ BhushanOnl9BkStr/BhushanOnl9BkStr/src/main/java/com/fullStack/BhushanOnl9BkStr/repository/BookRepository.java
@@ -0,0 +1,8 @@
+package com.fullStack.BhushanOnl9BkStr.repository;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class BookRepository {
+
+}
